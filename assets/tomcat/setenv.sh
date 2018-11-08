bdc57ea52847f48a205d44e30a1ed068165b197a
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2018-11-08 13:03:49 +0000

    Better logging

--- assets/tomcat/setenv.sh
+++ assets/tomcat/setenv.sh
@@ -5,7 +5,7 @@ JAVA_OPTS="$JAVA_OPTS -Dalfresco.home=/usr/local/alfresco"
 JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF-8"
 JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote"
 JAVA_OPTS="$JAVA_OPTS -XX:ReservedCodeCacheSize=1024m"
-JAVA_OPTS="$JAVA_OPTS -Xms1024m -Xmx8500m -XX:+UseG1GC -Djavax.net.ssl.trustStore=/data/truststore.jks" # java-memory-settings
+JAVA_OPTS="$JAVA_OPTS -Xms4096m -Xmx22500m -XX:+UseG1GC -Djavax.net.ssl.trustStore=/data/truststore.jks" # java-memory-settings
 
 # service:jmx:rmi:///jndi/rmi://192.168.1.33:9999/jmxrmi
 #JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.rmi.port=9999"
