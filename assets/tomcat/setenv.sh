faa582911fd2a3b4fd41e34dd066de2fb3d9b9cf
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2019-01-14 11:03:58 +0000

    Update setenv.sh

--- assets/tomcat/setenv.sh
+++ assets/tomcat/setenv.sh
@@ -5,6 +5,6 @@ JAVA_OPTS="$JAVA_OPTS -Dalfresco.home=/usr/local/alfresco"
 JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF-8"
 JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote"
 JAVA_OPTS="$JAVA_OPTS -XX:ReservedCodeCacheSize=1024m"
-JAVA_OPTS="$JAVA_OPTS -Xms4096m -Xmx17500m -XX:+UseG1GC -Djavax.net.ssl.trustStore=/data/truststore.jks" # java-memory-settings
+JAVA_OPTS="$JAVA_OPTS -Xms4096m -Xmx10500m -XX:+UseG1GC -Djavax.net.ssl.trustStore=/data/truststore.jks" # java-memory-settings
 
 export JAVA_OPTS
